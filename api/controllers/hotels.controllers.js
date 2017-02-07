2e28e1b5f28a4b704d89b8810957ced38d6a75b6
Ruoyun Daisy Pang <daisypang@Ruoyuns-MacBook-Pro.local>
2017-02-06 17:29:40 -0800

    hotelsGetAll and hotelsGetOne from our mongodb rather than hardcoded hotels.json

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -1,14 +1,17 @@
 // because it is a json file, we can just require() it
 // Or, we have to require("file") and then open and read that file asyncly.
 var hotelData = require("../data/hotel-data.json");
+
 var dbconn = require('../data/dbconnection.js');
+var ObjectId = require('mongodb').ObjectId;
 
 
 module.exports.hotelsGetAll = function(req, res) {
 	// get the db from dbconnection
 	var db = dbconn.get();
+	var collection = db.collection('hotels');
 
-	console.log("db", db);
+	console.log("Get db", db);
 	console.log("GET the hotels data");
 
 	// 'req.query' passes querystring into controllers
@@ -18,45 +21,57 @@ module.exports.hotelsGetAll = function(req, res) {
 	var offset = 0;
 	var count = 5;
 
-	// get query-offset data from 'req' obj
+	// user defined offset and count passed in as url querystring
 	if (req.query && req.query.offset) {
 		// parse the req.query.offset from string to int (string, radix)
 		offset = parseInt(req.query.offset, 10);
 	}
 
-	// get query-count data from 'req' obj
 	if (req.query && req.query.count) {
 		// parse the req.query.offset from string to int (string, radix)
 		count = parseInt(req.query.count, 10);
 	}
 
-	var returnData = hotelData.slice(offset, offset + count);
-
-	res
-		.status(200)
-		.json(returnData);
+	collection
+		.find()	// returns cursor obj which can yield json obj
+		.skip(offset)
+		.limit(count)
+		// asynchronously convert the cursor obj to json array
+		.toArray(function (err, docs) {
+			res
+				.status(200)
+				.json(docs);			
+		});
 };
 
 module.exports.hotelsGetOne = function(req, res) {
 	// get the db from dbconnection
 	var db = dbconn.get();
+	var collection = db.collection("hotels");
 
-	console.log("db", db);
+	console.log("Get db");
 
 	var hotelID = req.params.hotelID;
+
+	collection
+		// query for a single doc by its ObjectId asynchronously
+		.findOne({
+			_id : ObjectId(hotelID)
+		}, function (err, doc) {
+			res
+				.status(200)
+				.json(doc);
+		});
+
 	var thisHotel = hotelData[hotelID];
 
-	console.log("GET hotelID: ", hotelID);
-	res
-		.status(200)
-		.json(thisHotel);
 };
 
 module.exports.hotelsAddOne = function(req, res) {
 	// get the db from dbconnection
 	var db = dbconn.get();
 
-	console.log("db", db);
+	console.log("Get db");
 
 	console.log("POST new hotel");
 	console.log(req.body);
