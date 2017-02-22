ec1cf938b2aaac047b80a28ecbb29088f322eb6e
Ruoyun Daisy Pang <daisypang@Ruoyuns-MBP.hsd1.ca.comcast.net>
2017-02-21 18:48:25 -0800

    add controller -- reviewsUpdateOne

--- api/controllers/reviews.controllers.js
+++ api/controllers/reviews.controllers.js
@@ -40,7 +40,7 @@ module.exports.reviewsGetOne = function(req, res) {
 	var hotelID = req.params.hotelID;
 	var reviewID = req.params.reviewID;
 
-	console.log("GET reviewID " + reviewID + "for hotelID " + hotelID);
+	console.log("GET reviewID " + reviewID + " for hotelID " + hotelID);
 
 	Hotel
 		.findById(hotelID)
@@ -48,22 +48,30 @@ module.exports.reviewsGetOne = function(req, res) {
 		.exec(function(err, hotel) {
 			var response = {
 				status : 200,
-				message : hotel
+				message : {}
 			};
 
 			if (err) {
+				console.log("Error finding hotel", hotelID);
 				response.status = 500;
 				response.message = err;
 			}
-			else if (!doc) {
+			else if (!hotel) {
 				response.status = 404;
 				response.message = {
 					"message" : "HotelID not found"
-				}
+				};
 			}
 			else {
-				console.log("Returned hotel", hotel);
+				console.log("Returned review:" + reviewID + " of hotel:" + hotel);
 				var review = hotel.reviews.id(reviewID);
+				if (!review) {
+					console.log("reviewID of the hotel not found!");
+					response.status = 404;
+					response.message = {
+            			"message" : "Review ID not found " + reviewId
+          			};
+				}
 				response.message = review;
 			}
 
@@ -130,5 +138,80 @@ module.exports.reviewsAddOne = function(req, res) {
 					.status(response.status)
 					.json(response.message);
 			}
-});
+		});
+};
+
+var _updateReview = function(req, res, hotel, thisReview) {
+	console.log("Updating review of hotel:" + hotel);
+	thisReview.name = req.body.name;
+	thisReview.rating = parseInt(req.body.rating, 10);
+	thisReview.review = req.body.review;
+
+	// Use Mongoose save() method to save the model
+	hotel.save(function(err, hotelUpdated) {
+		if (err) {
+			console.log("Error saving changes to review:" + reviewID + "of hotel:" + hotel);
+			res
+				.status(500)
+				.json(err);
+		}
+		else {
+			res
+				.status(204)
+				.json();
+		}
+	});
+};
+
+// Update One review for hotelID
+module.exports.reviewsUpdateOne = function(req, res) {
+	var hotelID = req.params.hotelID;
+	var reviewID = req.params.reviewID;
+
+	console.log("GET reviewID " + reviewID + " for hotelID " + hotelID);
+
+	Hotel
+		.findById(hotelID)
+		.select("reviews")	// Telling MongoDB to only return reviews, which saves bandwidth
+		.exec(function(err, hotel) {
+			var thisReview;
+			var response = {
+				status : 200,
+				message : {}
+			};
+
+			if (err) {
+				console.log("Error finding hotel", hotelID);
+				response.status = 500;
+				response.message = err;
+			}
+			else if (!hotel) {
+				console.log("HotelID" + hotelID + "not found!");
+				response.status = 404;
+				response.message = {
+					"message" : "HotelID not found"
+				};
+			}
+			else {
+				// Get the review
+				thisReview = hotel.reviews.id(reviewID);
+				// If the review doesn't exists Mongoose returns null
+				if (!thisReview) {
+					console.log("reviewID: " + reviewID + " of the hotel not found");
+					response.status = 404;
+					response.message = {
+            			"message" : "Review ID not found " + reviewId
+          			};
+				}
+			}
+
+			if (response.status !== 200) {
+				res
+					.status(response.status)
+					.json(response.message);
+			}
+			else {
+				_updateReview(req, res, hotel, thisReview);
+			}
+		});
 };
