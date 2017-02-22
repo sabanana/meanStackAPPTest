03aa5012ba3180eb2005b26ce7c09e7c871c8a23
Ruoyun Daisy Pang <daisypang@Ruoyuns-MBP.hsd1.ca.comcast.net>
2017-02-21 16:14:19 -0800

    modify routes for posting a new hotel/review

--- api/routes/index.js
+++ api/routes/index.js
@@ -6,29 +6,27 @@ var ctrlHotels = require("../controllers/hotels.controllers.js");
 var ctrlReviews = require("../controllers/reviews.controllers.js");
 
 // GET/POST hotels
-// 1. Get All Hotels
+// 1. Get All Hotels OR Add One Hotel using POST method
 router
 	.route('/hotels')
-
 	// GET method; callback controller
-	.get(ctrlHotels.hotelsGetAll);
+	.get(ctrlHotels.hotelsGetAll)
+	// POST a new hotel
+	.post(ctrlHotels.hotelsAddOne);
 
 // 2. Get a hotel by its ID
 router
 	.route('/hotels/:hotelID')
 	.get(ctrlHotels.hotelsGetOne);
 
-// 3. Add a hotel by POST an HTML form
-router
-	.route('/hotels/new')
-	.post(ctrlHotels.hotelsAddOne);
 
 
 // GET/POST reviews
-// 1. Get all reviews for a hotel
+// 1. Get all reviews for a hotel OR Post a new review for the hotel
 router
 	.route('/hotels/:hotelID/reviews')
-	.get(ctrlReviews.reviewsGetAll);
+	.get(ctrlReviews.reviewsGetAll)
+	.post(ctrlReviews.reviewAddOne);
 
 // 2. Get a single review for a hotel
 router
