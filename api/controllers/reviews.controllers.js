6e613b222e25aa5cb13879b121de90ef1490ee53
Ruoyun Daisy Pang <daisypang@Ruoyuns-MBP.hsd1.ca.comcast.net>
2017-02-21 16:37:18 -0800

    add controller: reviewsAddOne to POST a review for the hotel queried

--- api/controllers/reviews.controllers.js
+++ api/controllers/reviews.controllers.js
@@ -72,3 +72,63 @@ module.exports.reviewsGetOne = function(req, res) {
 				.json(response.message);
 		});
 };
+
+var _addReview = function(req, res, hotel) {
+	hotel.reviews.push({
+		name : req.body.name,
+		rating : parseInt(req.body.rating, 10),
+		review : req.body.review
+	});
+
+	// Use Mongoose save() method to save the model
+	hotel.save(function(err, hotelUpdated) {
+		if (err) {
+			res
+				.status(500)
+				.json(err);
+		}
+		else {
+			res
+				.status(201)
+				.json(hotelUpdated.reviews[hotelUpdated.reviews.length-1]);
+		}
+	});
+};
+
+// Add One review for hotelID
+module.exports.reviewsAddOne = function(req, res) {
+	var hotelID = req.params.hotelID;
+	console.log("Get hotelID", hotelID);
+
+	Hotel
+		.findById(hotelID)
+		.select('reviews')
+		.exec(function(err, doc) {
+			var response = {
+				status : 200,
+				message : []
+			};
+
+			if (err) {
+				console.log("Error finding hotel");
+				response.status = 500;
+				response.message = err;
+			}
+			else if (!doc) {
+				console.log("Hotel id not found in database", id);
+				response.status = 404;
+				response.message = {
+					"message" : "Hotel ID not found" + id					
+				};
+			}
+
+			if (doc) {
+				_addReview(req, res, doc);	
+			}
+			else {
+				res
+					.status(response.status)
+					.json(response.message);
+			}
+});
+};
