a8dfe181d58d35de88a30176937108032ac109ea
Ruoyun Daisy Pang <daisypang@Ruoyuns-MBP.hsd1.ca.comcast.net>
2017-02-24 00:01:40 -0800

    modified: add function for handling review submitting -- addReview()

--- public/angular-app/hotel-display/hotel-display-controller.js
+++ public/angular-app/hotel-display/hotel-display-controller.js
@@ -1,13 +1,38 @@
 angular.module('meanhotel').controller('HotelController', HotelController);
 
-function HotelController($routeParams, hotelDataFactory) {
+function HotelController($route, $routeParams, hotelDataFactory) {
 	var vm = this;
 	var id = $routeParams.id;
+	vm.isSubmitted = false;		// form submitted unsuccessfully
 
 	hotelDataFactory.hotelDisplay(id).then(function(res) {
-		vm.hotel = res;
-		vm.stars = _getStarRating(res.stars);
+		vm.hotel = res.data;
+		vm.stars = _getStarRating(res.data.stars);
 	});
+
+	vm.addReview = function() {
+		var postData = {
+			name : vm.name,
+			rating : vm.rating,
+			review : vm.review
+		};
+
+		if (vm.reviewForm.$valid) {
+			hotelDataFactory.postReview(id, postData).then(function(response) {
+				console.log(response.status);
+				if (response.status === 201) {
+					// reload the page automatically if submit successfully
+					$route.reload();
+				}
+			}).catch(function(error) {
+				console.log(error);
+			});
+		}
+		else {
+			console.log('invalid form');
+			vm.isSubmitted = true;	// submit unsuccessfully
+		}
+	};
 }
 
 function _getStarRating(stars) {
