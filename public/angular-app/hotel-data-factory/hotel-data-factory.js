a43f97ab621d8246b2d965625d37a803b9315089
Ruoyun Daisy Pang <daisypang@Ruoyuns-MBP.hsd1.ca.comcast.net>
2017-02-24 00:03:20 -0800

    modified: add a new function postReview to posting a review to backend

--- public/angular-app/hotel-data-factory/hotel-data-factory.js
+++ public/angular-app/hotel-data-factory/hotel-data-factory.js
@@ -3,7 +3,8 @@ angular.module('meanhotel').factory('hotelDataFactory', hotelDataFactory);
 function hotelDataFactory($http) {
 	return {
 		hotelList : hotelList,
-		hotelDisplay : hotelDisplay
+		hotelDisplay : hotelDisplay,
+		postReview : postReview
 	};
 
 	function hotelList() {
@@ -14,8 +15,12 @@ function hotelDataFactory($http) {
 		return $http.get('/api/hotels/' + id).then(complete).catch(failed);
 	}
 
+	function postReview(id, postData) {
+		return $http.post('api/hotels/' + id + '/reviews', postData).then(complete).catch(failed);
+	}
+
 	function complete(response) {
-		return response.data;
+		return response;
 	}
 
 	function failed(error) {
